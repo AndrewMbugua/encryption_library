50f8ba36a7bee248c929f21e514309dab9840e52
AndrewMbugua <andrewmbugua388@gmail.com>
2023-01-31 12:40:35 +0300

    Scripted auto-commit on change (2023-01-31 12:40:35) by gitwatch.sh

--- float_double_to_IEEE-754_format.c
+++ float_double_to_IEEE-754_format.c
@@ -6,6 +6,7 @@
 
 #include <stdlib.h>
 #include <stdint-gcc.h>
+#include <stdio.h>
 
 #define pack754_32(f) (pack754((f), 32, 8))
 #define pack754_64(f) (pack754((f), 64, 11))
@@ -73,10 +74,28 @@ long double unpack754(uint64_t i, unsigned bits, unsigned expbits)
 // Usage demonstration
 int main(){
 
+    float f = 3.1415926, f2;
+    double d = 3.14159265358979323, d2;
+    uint32_t fi;
+    uint64_t di;
+
+    fi = pack754_32(f);
+    f2 = unpack754_32(fi);
+
+    di = pack754_64(d);
+    d2 = unpack754_64(di);
+
+    printf("float before : %.7f\n", f);
+    printf("float encoded: 0x%08" PRIx32 "\n", fi);
+    printf("float after : %.7f\n\n", f2);
 
 
+    printf("double before : %.20lf\n", d);
+    printf("double encoded: 0x%016" PRIx64 "\n", di);
+    printf("double after : %.20lf\n", d2);
 
 
+    return 0;
 
 
 }
