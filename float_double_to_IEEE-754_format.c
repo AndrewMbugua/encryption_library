da17d379a7bf999a18d3f8256f2336ba48a24721
AndrewMbugua <andrewmbugua388@gmail.com>
2023-01-31 12:34:44 +0300

    Scripted auto-commit on change (2023-01-31 12:34:44) by gitwatch.sh

--- float_double_to_IEEE-754_format.c
+++ float_double_to_IEEE-754_format.c
@@ -18,41 +18,24 @@ uint64_t pack754(long double f, unsigned bits, unsigned expbits)
 // check sign and begin normalization
     if (f < 0) { sign = 1; fnorm = -f; }
     else { sign = 0; fnorm = f; }
-    15
-    16
-    17
-    18
+
 // get the normalized form of f and track the exponent
     shift = 0;
     while(fnorm >= 2.0) { fnorm /= 2.0; shift++; }
     while(fnorm < 1.0) { fnorm *= 2.0; shift--; }
     fnorm = fnorm - 1.0;
-    19
-    20
-    21
-    22
-    23
-    24
+
 // calculate the binary form (non-float) of the significand data
     significand = fnorm * ((1LL<<significandbits) + 0.5f);
-    25
-    26
-    27
+
 // get the biased exponent
     exp = shift + ((1<<(expbits-1)) - 1); // shift + bias
-    28
-    29
-    30
+
 // return the final answer
     return (sign<<(bits-1)) | (exp<<(bits-expbits-1)) | significand;
-    31
-    32
-    33
+
 }
-34
-35
-36
-37
+
 long double unpack754(uint64_t i, unsigned bits, unsigned expbits)
 {
     long double result;
