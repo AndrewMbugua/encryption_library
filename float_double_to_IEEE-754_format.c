346ef3a9d70a37a953c663b50d6911750398830f
AndrewMbugua <andrewmbugua388@gmail.com>
2023-01-31 12:33:55 +0300

    Scripted auto-commit on change (2023-01-31 12:33:55) by gitwatch.sh

--- float_double_to_IEEE-754_format.c
+++ float_double_to_IEEE-754_format.c
@@ -2,3 +2,7 @@
 // Created by andrew mbugua on 1/31/23.
 //
 #define pack754_32(f) (pack754((f), 32, 8))
+#define pack754_64(f) (pack754((f), 64, 11))
+#define unpack754_32(i) (unpack754((i), 32, 8))
+#define unpack754_64(i) (unpack754((i), 64, 11))
+
